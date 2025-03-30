25e1c346a6212cd55a24b188ded00b3d3f49332e
Linh Pham <linhmaipham@usf.edu>
2025-03-30 15:20:44 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -9,3 +9,15 @@ function fetchProductsThen() {
         .catch(handleError);
 }
 
+
+// Task 3: Fetch Products with async/await
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch('https://www.course-api.com/javascript-store-products');
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
